554653e18ea2ce0208b9700c7aab29493fa521f7
Josue Alfaro <yoshua_jaav@hotmail.com>
2025-09-17 22:39:14 -0500

    add datable component and configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataTableComponent } from './components/data-table/data-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, DataTableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyecto-aseguradora';
+  title = 'mi-proyecto-angular';
 }
